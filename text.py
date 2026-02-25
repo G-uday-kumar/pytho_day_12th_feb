924bd860495119da4e85dafb5f6a3352fbf89d95
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-02-25 23:10:43 +0530

    Add files via upload

---
+++ text.py
@@ -0,0 +1,21 @@
+def add():
+    a=10
+    b=20
+    c=a+b
+    print("the  sum is",c)
+add()
+def sub(20,5):
+    c=a-b
+    return "the sub is",c
+print(sub(a,b))
+def mul():
+    a=10
+    b=20
+    c=a*b
+    return"multiplication is ",c
+print(mul())
+
+def div(a,b):
+    c=a/b
+    print("the division is",c)
+div(10,20)
---
+++ train.py
@@ -0,0 +1,10 @@
+class Train:
+    count=100
+    def __init__(self):
+        Train.count-=1
+        print("the total reamining number of seats are:-",Train.count)
+t1=Train()
+t2=Train()
+t3=Train()
+t4=Train()
+t5=Train()
