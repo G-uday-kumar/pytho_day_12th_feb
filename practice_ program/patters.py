f26411f0b98d787981ccc120bdd93c7701d5225c
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-09 21:01:37 +0530

    Add files via upload

---
+++ practice_ program/patters.py
@@ -0,0 +1,110 @@
+if __name__=="__main__":
+    # row=int(input("enter your no of rowa:-"))
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1):
+    #         print(i,end=" ")
+    #     print()
+        # enter your no of rowa:-3
+        # 1 1 1
+        # 2 2 2
+        # 3 3 3
+    # for i in range(1,row+1):
+    #     for j in range(1,i+1):
+    #         print("*",end=" ")
+    #     print()
+        # enter your no of rowa:-5
+        # *
+        # * *
+        # * * *
+        # * * * *
+        # * * * * *
+    # for i in range(1,row+1):
+    #     for j in range(1,i+1):
+    #         print(j,end=" ")
+    #     print()
+    # enter your no of rowa:-5
+    # 1
+    # 1 2
+    # 1 2 3
+    # 1 2 3 4
+    # 1 2 3 4 5
+
+    # for i in range(1,row+1):
+    #     for j in range(i,1-1,-1):
+    #         print(j,end=" ")
+    #     print()
+    # enter your no of rowa:-5
+    # 1
+    # 2 1
+    # 3 2 1
+    # 4 3 2 1
+    # 5 4 3 2 1
+
+    # for i in range(1,row+1):
+    #     for j in range(row,row+1-i-1,-1):
+    #         print(j,end=" ")
+    #     print()
+    # enter your no of rowa:-5
+    # 5
+    # 5 4
+    # 5 4 3
+    # 5 4 3 2
+    # 5 4 3 2 1
+    row=5
+    # for i in range(1,row+1):
+    #     for j in range(1,row-i+1):
+    #         print("_",end=" ")
+    #     for j in range(1,i+1):
+    #         print(j,end=" ")
+    #     print()
+#     for i in range(1,row+1):
+#         for j in range(1,row-i+1):
+#             print("_",end=" ")
+#         for j in range(1,i+1):
+#             print(j,end=" ")
+#         print()
+# _ _ _ _ 1
+# _ _ _ 1 2
+# _ _ 1 2 3
+# _ 1 2 3 4
+# 1 2 3 4 5
+
+    # row=5
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print("_",end=" ")
+    #     for j in range(row,row+1-i-1,-1):
+    #         print(j,end=" ")
+    #     print()
+# _ _ _ _ 5
+# _ _ _ 5 4
+# _ _ 5 4 3
+# _ 5 4 3 2
+# 5 4 3 2 1
+
+    # row=5
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print(" ",end=" ")
+    #     for j in range(1,i+1):
+    #         print(j,end=" ")
+    #     print()
+#         1
+#       1 2
+#     1 2 3
+#   1 2 3 4
+# 1 2 3 4 5
+
+    # row=5
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print(" ",end=" ")
+    #     for j in range(row,row+1-i-1,-1):
+    #         print(j,end=" ")
+    #     print()
+#         5
+#       5 4
+#     5 4 3
+#   5 4 3 2
+# 5 4 3 2 1
+
