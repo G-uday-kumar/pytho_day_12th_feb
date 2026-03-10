5210ad5e9db17ba8c3fb6fa867876645dc8ef740
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-03-10 22:38:17 +0530

    Add files via upload

---
+++ practice_ program/pattern1.py
@@ -0,0 +1,54 @@
+if __name__=="__main__":
+    row=5
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print(" ",end=" ")
+    #     for j in range(1,i+1):
+    #         print("*",end=" ")
+    #     for j in range(1,i):
+    #         print("*",end=" ")
+    #     print()
+        #         *
+        #       * * *
+        #     * * * * *
+        #   * * * * * * *
+        # * * * * * * * * *
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print(" ",end=" ")
+    #     for j in range(row-i+1,row+1):
+    #         print(j,end=" ")
+    #     for j in range(row-1,row-i,-1):
+    #         print(j,end=" ")
+    #     print()
+    #         5
+    #       4 5 4
+    #     3 4 5 4 3
+    #   2 3 4 5 4 3 2
+    # 1 2 3 4 5 4 3 2 1
+    # for i in range(1,row+1):
+    #     for j in range(1,row+1-i):
+    #         print(" ",end=" ")
+    #     for j in range(i,0,-1):
+    #         print(j,end=" ")
+    #     for j in range(2,i+1):
+    #         print(j,end=" ")
+    #     print()
+    #         1
+    #       2 1 2
+    #     3 2 1 2 3
+    #   4 3 2 1 2 3 4
+    # 5 4 3 2 1 2 3 4 5
+    for i in range(1,row+1):
+        for j in range(1,row+1-i):
+            print(" ",end=" ")
+        for j in range(1,i+1):
+            print(j,end=" ")
+        for j in range(i,0,-1):
+            print(j,end=" ")
+        print()
+    #         1 1
+    #       1 2 2 1
+    #     1 2 3 3 2 1
+    #   1 2 3 4 4 3 2 1
+    # 1 2 3 4 5 5 4 3 2 1
