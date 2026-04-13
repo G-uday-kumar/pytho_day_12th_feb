3f3cafdb00fce8f0f91ab925c048b7a0dfdf42b3
G UDAY KUMAR <156932642+G-uday-kumar@users.noreply.github.com>
2026-04-13 23:25:40 +0530

    Add files via upload

---
+++ Dict.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    arr=[10,10,10,20,30,20,20,20,20,30]
+
+    settt={10,10,10,20,30,20,20,20,20,30}
+    print(type(settt))
+    tple=(10,10,10,20,30,20,20,20,20,30)
+    print(type(tple))
+    dict={}
+    print(type(dict))
+
+    print(type(arr))
+    dict[10]=3
+    dict[20]=5
+    dict[30]=2
+    print(dict)
